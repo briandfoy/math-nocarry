da848afa80c1f9823a2696d014bafd7d4158a3b4
brian d foy <brian.d.foy@gmail.com>
2002-06-25 18:59:15 +0000

    * added documentation
    * fixed some documentation format issues
    
    * added checks in add and subtract to only deal with
    positive numbers (for now)
    
    * added correct sign handling in multiply
    
    
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@166 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/NoCarry.pm
+++ lib/NoCarry.pm
@@ -14,18 +14,64 @@ Math::NoCarry - Perl extension for no carry arithmetic
 
 	use Math::NoCarry;
 
-	my $sum     = Math::NoCarry::add( 123, 456 );
+	my $sum        = Math::NoCarry::add( 123, 456 );
+
+	my $difference = Math::NoCarry::subtract( 123, 456 );
 	
-	my $product = Math::NoCarry::multiply( 123, 456 );
+	my $product    = Math::NoCarry::multiply( 123, 456 );
 		
 =head1 DESCRIPTION
 
+No carry arithmetic doesn't allow you to carry digits to the
+next column.  For example, if you add 8 and 4, you normally
+expect the answer to be 12, but that 1 digit is a carry.
+In no carry arithmetic you can't do that, so the sum of
+8 and 4 is just 2.  In effect, this is addition modulo 10
+in each column. I discard all of the carry digits in
+this example:
+
+	  1234
+	+ 5678
+	------
+	  6802
+	  
+For multiplication, the result of pair-wise multiplication
+of digits is the modulo 10 value of their normal, everyday
+multiplication.
+
+        123
+      x 456
+      -----
+          8   6 x 3 
+         2    6 x 2
+        6     6 x 1
+        
+         5    5 x 3
+        0     5 x 2
+       5      5 x 1
+     
+        2     4 x 3
+       8      4 x 2
+    + 4       4 x 1
+    -------
+      43878
+    
+Since multiplication and subtraction are actually types of
+additions, you can multiply and subtract like this as well.
+
+No carry arithmetic is both associative and commutative.
 
 =head1 FUNCTIONS
+
+This module does not export any functions.
 
 =over 4
 
 =item multiply( A, B )
+
+Returns the no carry product of A and B.
+
+Return A if it is the only argument ( A x 1 );
 
 =cut
 
@@ -33,8 +79,13 @@ sub multiply
 	{
 	return $_[0] if $#_ < 1;
 	
-	my @p0 = reverse split //, $_[0];
-	my @p1 = reverse split //, $_[1];
+	@_ = map { $_ += 0 } @_;
+	
+	my $sign = ($_[0] > 0 and $_[1] < 0 ) ||
+		($_[1] > 0 and $_[0] < 0 );
+				
+	my @p0 = reverse split //, abs $_[0];
+	my @p1 = reverse split //, abs $_[1];
 	
 	my @m;
 		
@@ -50,12 +101,19 @@ sub multiply
 		{		
 		unshift @m, Math::NoCarry::add( shift @m, shift @m );
 		}
-		
+	
+	$m[0] *= -1 if $sign;
+	
 	return $m[0];	
 	}
 
 =item add( A, B )
 
+Returns the no carry sum of the positive numbers A and B.
+
+Returns A if it is the only argument ( A + 0 )
+
+Returns undef or the empty list if either number is negative.
 
 =cut
 	
@@ -63,6 +121,10 @@ sub add
 	{
 	return $_[0] if $#_ < 1;
 
+	@_ = map { local $^W; $_ += 0 } @_;
+	
+	return unless( $_[0] >= 0 and $_[1] >= 0 );
+	
 	my @addends = map scalar reverse, @_;
 		
 	my $string = '';
@@ -86,10 +148,22 @@ sub add
 	return $string;
 	}
 
+=item add( A, B )
+
+Returns the no carry difference of the postive numbers A and B.
+
+Returns A if it is the only argument ( A - 0 )
+
+Returns undef or the empty list if either number is negative.
+
+=cut
+	
 sub subtract
 	{
 	return $_[0] if $#_ < 1;
 
+	return unless( $_[0] >= 0 and $_[1] >= 0);
+	
 	my @addends = map scalar reverse, @_;
 	
 	my $string = '';
@@ -112,11 +186,33 @@ sub subtract
 	
 	return $o;
 	}
+	
 1;
+
 __END__
+
+=head1 BUGS
+
+* none reported yet :)
+
+=head1 TO DO
+
+* this could be a full object package with overloaded
++, *, and - operators
+
+* it would be nice if i could give the functions more than
+two arguments.
+
+* addition and subtraction don't do negative numbers.
 
 =head1 AUTHOR
 
 brian d foy, <bdfoy@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright 2002, brian d foy, All Rights Reserved.
+
+You may redistribute this under the same terms as Perl itself.
 
 =cut
