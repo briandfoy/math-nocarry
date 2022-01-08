cbc6c398dbe76a3c5db571ed43bf6d477003744c
brian d foy <brian.d.foy@gmail.com>
2022-01-08 17:22:51 -0500

    bump version

--- lib/Math/NoCarry.pm
+++ lib/Math/NoCarry.pm
@@ -9,7 +9,7 @@ use Exporter qw(import);
 our @EXPORT_OK = qw(add subtract multiply);
 our %EXPORT_TAGS =  ( all => [ @EXPORT_OK ] );
 
-our $VERSION = '1.113';
+our $VERSION = '1.114';
 
 =encoding utf8
 
